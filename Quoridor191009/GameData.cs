5ef9cbff4548e9fd29e43568a86187c2876eec2e
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2019-10-12 16:15:40 +0200

    added startcode

---
+++ Quoridor191009/GameData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+
+//Brädet är numrerat från nedre vänstra hörnet (0,0) till övre högra hörnet (8,8)
+//Spelaren går alltid uppåt dvs (-,8) är målraden.
+
+public enum Typ { Flytta, Horisontell, Vertikal }
+public struct Drag {
+    public Typ typ;
+    public Point point;
+}
+public class SpelBräde {
+    public const int N = 9;
+    public bool avanceradeRegler;       //Normalt false
+    public List<Spelare> spelare;       //Spelaren är alltid först i listan.
+
+    //Horisontella väggar, om (x,y) är satt
+    // så kan spelaren inte gå från (x,y) till (x,y+1)
+    public bool[,] horisontellaVäggar = new bool[N, N - 1];  //9*8
+
+    //Vertikala väggar, om (x,y) är satt
+    // så kan spelaren inte gå från (x,y) till (x+1,y)
+    public bool[,] vertikalaVäggar = new bool[N - 1, N]; //8*9
+
+    #region Överkurs
+    //Om man vill hindra väggarna från att korsas.
+    //Horisontella långa väggar, om (x,y) är satt 
+    // så kan spelaren inte gå från(x, y) till(x, y+1)
+    //          och inte från (x+1, y) till (x+1, y+1)
+    public bool[,] horisontellaLångaVäggar = new bool[N - 1, N - 1]; //8*8
+
+    //Vertikala långa väggar, om (x,y) är satt
+    // så kan spelaren inte gå från (x,y) till (x+1,y)
+    //          och inte från (x, y+1) till (x+1, y+1)
+    public bool[,] vertikalaLångaVäggar = new bool[N - 1, N - 1]; //8*8
+    #endregion Överkurs
+    public SpelBräde() {    }
+}
+public enum Färg { Röd, Blå };  //Röd börjar alltid
+public class Spelare {
+    public Point position;  //Platsen spelaren eller väggen ska placeras
+    public Färg färg;
+    public int antalVäggar; //Antal väggar kvar att placera ut
+}
---
+++ Quoridor191009/QGameProj/Student/Agent.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+class Agent:BaseAgent {
+    [STAThread]
+    static void Main() {
+        Program.Start(new Agent());
+    }
+    public Agent() { }
+    public override Drag SökNästaDrag(SpelBräde bräde) {
+        //Så ni kan kolla om systemet fungerar!
+        Spelare jag = bräde.spelare[0];
+        Point playerPos = jag.position;
+        Drag drag = new Drag();
+
+
+
+        if (jag.antalVäggar == 10)
+        {
+            drag.typ = Typ.Horisontell;
+            drag.point = new Point(3, 6);
+        }
+        else if (jag.antalVäggar == 9)
+        {
+            drag.typ = Typ.Horisontell;
+            drag.point = new Point(5, 6);
+        }
+        else
+        {
+            drag.typ = Typ.Flytta;
+            drag.point = playerPos;
+            drag.point.Y++;
+        }
+        return drag;
+    }
+    public override Drag GörOmDrag(SpelBräde bräde, Drag drag) {
+        //Om draget ni försökte göra var felaktigt så kommer ni hit
+        System.Diagnostics.Debugger.Break();    //Brytpunkt
+        return SökNästaDrag(bräde);
+    }
+
+    //public void CreateGraph()
+    //{
+    //    Point[,] posArray = new Point[8, 8];
+
+    //    for (int i = 0; i < 8; i++)
+    //    {
+    //        for (int j = 0; j < 8; j++)
+    //        {
+    //            if (i + 1 > 8 || j + 1 > 8 || i - 1 < 0 || j - 1 < 0)
+    //            {
+    //                //Utanför brädan
+    //            }
+    //        }
+    //    }
+    //}
+
+    public void Graph()
+    {
+
+    }
+}
+
+//enum Typ { Flytta, Horisontell, Vertikal }
+//struct Drag
+//{
+//    public Typ typ;
+//    public Point point;
+//}
