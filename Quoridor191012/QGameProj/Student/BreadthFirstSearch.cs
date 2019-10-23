d2c4788d8872a22a9173c98156895e7bb362b440
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2019-10-23 22:49:52 +0200

    added breadthfirstsearch class

---
+++ Quoridor191012/QGameProj/Student/BreadthFirstSearch.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+
+class BreadthFirstSearch
+{
+    private bool[,] marked;
+    private Point[,] edgeTo;
+    private Point s;
+
+    public BreadthFirstSearch(Graph G, Point s)
+    {
+        marked = new bool[G.V, G.V];
+        edgeTo = new Point[G.V, G.V];
+        this.s = s;
+        bfs(G, s);
+    }
+
+    private void bfs(Graph G, Point s)
+    {
+        Queue<Point> queue = new Queue<Point>();
+        marked[s.X, s.Y] = true;
+        queue.Enqueue(s);
+
+        while (queue.Any())
+        {
+            Point v = queue.Dequeue();
+
+            foreach (Point p in G.adj[v.X,v.Y])
+            {
+                if (!marked[p.X, p.Y])
+                {
+                    edgeTo[p.X, p.Y] = v;
+                    marked[p.X, p.Y] = true;
+                    queue.Enqueue(p);
+                }
+            }
+        }
+    }
+
+    public IEnumerable <Point> PathTo(Point v)
+    {
+        if (!HasPathTo(v))
+        {
+            return null;
+        }
+        Stack<Point> path = new Stack<Point>();        
+        for (Point x = v; x != s;x = edgeTo[x.X,x.Y])
+        {
+            path.Push(x);
+        }
+        path.Push(s);
+        return path;
+    }
+
+    public bool HasPathTo(Point v)
+    {
+        return marked[v.X, v.Y];
+    }
+
+}
+
--- Quoridor191012/QGameProj/Student/Graph.cs
+++ Quoridor191012/QGameProj/Student/Graph.cs
@@ -7,15 +7,12 @@ using Microsoft.Xna.Framework;
 
 class Graph
 {
-    Point[,] posArray;
-    Point vertices;
-    List<Point>[,] adj;
-    int E;
-    int V;
+    public List<Point>[,] adj;
+    public int E;
+    public int V;
 
     public Graph(int V)
     {
-        vertices = new Point(V, V);
         adj = new List<Point>[V, V];
         E = 0;
         this.V = V;
@@ -33,7 +30,7 @@ class Graph
 
     void Startup()
     {
-        //  För alla rutor(R) i grafen.
+        //För alla rutor(R) i grafen.
         //• För alla omgivande rutor(4 stycken) A
         //• Kolla att A är inom spelbrädet
         //• Om R.X == A.X kolla att det inte finns en horisontell vägg
@@ -41,13 +38,13 @@ class Graph
         //• Om R.Y == A.Y kolla att det inte finns en vertikal vägg ivägen.
         //• Lägg till A till som angränsande ruta till R.
 
-        for (int i = 0; i < vertices.X; i++)
+        for (int i = 0; i < V; i++)
         {
-            for (int j = 0; j < vertices.Y; j++)
+            for (int j = 0; j < V; j++)
             {
-                for (int l = i - 1; l < i + 2; l++)
+                for (int l = i - 1; l < i + 2; l+=2)
                 {
-                    for (int k = j - 1; k < j + 2; k++)
+                    for (int k = j - 1; k < j + 2; k+=2)
                     {
                         if (l > 9 || l < 0 || k > 9 || k < 0 || i == l || j == l)
                         {
@@ -69,21 +66,5 @@ class Graph
         adj[w.X, w.Y].Add(v);
         ++E;
     }
-
-    //public void reverse()
-    //{
-    //    Graph R = new Graph(V);
-    //    for (int i = 0; i < V; i++)
-    //    {
-    //        for (int i = 0; i < V; i++)
-    //        {
-    //            for (int j = 0; j < V; j++)
-    //            {
-    //                R.AddEdge()
-    //            }
-    //        }
-    //    }
-    //}
-
-
+    
 }
