87c374dcdff83b9d34b1bfb8129d13f93174d825
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2019-10-29 14:41:23 +0100

    fixed bug for Ai making it step to the left before walking forward

--- Quoridor191012/QGameProj/Student/Agent.cs
+++ Quoridor191012/QGameProj/Student/Agent.cs
@@ -1,74 +1,113 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
+using System.Collections.Generic;
 
-class Agent:BaseAgent {
+class Agent :BaseAgent {
     [STAThread]
     static void Main() {
         Program.Start(new Agent());
     }
     public Agent() { }
     public override Drag SökNästaDrag(SpelBräde bräde) {
-        //Så ni kan kolla om systemet fungerar!
         Spelare jag = bräde.spelare[0];
         Point playerPos = jag.position;
         Drag drag = new Drag();
         Graph aiGraph = new Graph(9,bräde);
-        BreadthFirstSearch dragSearch = new BreadthFirstSearch(aiGraph, playerPos);
+        BreadthFirstSearch search = new BreadthFirstSearch(aiGraph, playerPos);
+        int currentSteps = 0;
+        int lowestSteps = 100;
+        int pathPos = 0;
+        Point lowestPathPos = new Point(0,0);
+
         for (int i = 0; i < 9; i++)
         {
-            foreach (Point x in dragSearch.PathTo(new Point(i,8)))
+            foreach (Point x in search.PathTo(new Point(i, 8)))
             {
-                //fixa till att den väljer den kortaste vägen till andra sidan av alla punkterna
-                if (x.X < playerPos.X)
-                {
-                    drag.typ = Typ.Flytta;
-                    drag.point = playerPos;
-                    drag.point.X--;
-                    return drag;
-                }
-                if (x.X > playerPos.X)
-                {
-                    drag.typ = Typ.Flytta;
-                    drag.point = playerPos;
-                    drag.point.X++;
-                    return drag;
-                }
-                if (x.Y < playerPos.Y)
-                {
-                    drag.typ = Typ.Flytta;
-                    drag.point = playerPos;
-                    drag.point.Y--;
-                    return drag;
-                }
-                if (x.Y > playerPos.Y)
-                {
-                    drag.typ = Typ.Flytta;
-                    drag.point = playerPos;
-                    drag.point.Y++;
-                    return drag;
-                }
+                ++currentSteps;
+            }
+
+            if (currentSteps < lowestSteps)
+            {
+                lowestSteps = currentSteps;
+                lowestPathPos = new Point(i, 8);
+                pathPos = i;
+            }
+            currentSteps = 0;
+        }
+
+        foreach (Point nextStep in search.PathTo(new Point(pathPos, 8)))
+        {
+            if (nextStep.X < playerPos.X)
+            {
+                drag.typ = Typ.Flytta;
+                drag.point = playerPos;
+                drag.point.X--;
+                return drag;
+            }
+            if (nextStep.X > playerPos.X)
+            {
+                drag.typ = Typ.Flytta;
+                drag.point = playerPos;
+                drag.point.X++;
+                return drag;
+            }
+            if (nextStep.Y < playerPos.Y)
+            {
+                drag.typ = Typ.Flytta;
+                drag.point = playerPos;
+                drag.point.Y--;
+                return drag;
+            }
+            if (nextStep.Y > playerPos.Y)
+            {
+                drag.typ = Typ.Flytta;
+                drag.point = playerPos;
+                drag.point.Y++;
+                return drag;
             }
         }
 
-        //if (jag.antalVäggar == 10) {
-        //    drag.typ = Typ.Horisontell;
-        //    drag.point = new Point(3, 6);
-        //}
-        //else if (jag.antalVäggar == 9) {
-        //    drag.typ = Typ.Horisontell;
-        //    drag.point = new Point(5, 6);
-        //}
-        //else {
-        //    drag.typ = Typ.Flytta;
-        //    drag.point = playerPos;
-        //    drag.point.Y++;
-        //}
         return drag;
     }
     public override Drag GörOmDrag(SpelBräde bräde, Drag drag) {
         //Om draget ni försökte göra var felaktigt så kommer ni hit
         System.Diagnostics.Debugger.Break();    //Brytpunkt
         return SökNästaDrag(bräde);
+    }
+
+    private Drag whatDirection(Point x,Point playerPos,Drag drag)
+    {
+        if (x.X < playerPos.X)
+        {
+            drag.typ = Typ.Flytta;
+            drag.point = playerPos;
+            drag.point.X--;
+            return drag;
+        }
+        if (x.X > playerPos.X)
+        {
+            drag.typ = Typ.Flytta;
+            drag.point = playerPos;
+            drag.point.X++;
+            return drag;
+        }
+        if (x.Y < playerPos.Y)
+        {
+            drag.typ = Typ.Flytta;
+            drag.point = playerPos;
+            drag.point.Y--;
+            return drag;
+        }
+        if (x.Y > playerPos.Y)
+        {
+            drag.typ = Typ.Flytta;
+            drag.point = playerPos;
+            drag.point.Y++;
+            return drag;
+        }
+
+        return drag;
+
     }
 }
 //enum Typ { Flytta, Horisontell, Vertikal }
